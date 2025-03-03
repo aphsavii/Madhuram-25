0d59aed259f8a7622f0c001595aaa8fffe010b15
sid1337 <siddharthbajpai007@protonmail.com>
2025-03-03 16:14:40 +0530

    Team Page commit #1 err resolve

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "@/components/Header";
+import Header from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import "./team.css";
 import { useState, useEffect } from "react";
