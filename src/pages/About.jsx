f8462bdb108691c32d93df88ae5680cb4e1516c8
Rahul Sharma <moudgil010@gmail.com>
2025-03-03 22:19:51 +0530

    About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,6 +12,7 @@ import group2 from "/assets/About/group2.png";
 import gpic1 from "/assets/About/gpic1.jpg";
 import gpic2 from "/assets/About/gpic2.png";
 import gpic3 from "/assets/About/gpic3.jpg";
+import FlowerImage from "/assets/74.png"
 import { transform } from "motion";
 
 function About() {
@@ -51,26 +52,24 @@ function About() {
 
   return (
     <div className="w-full bg-[#FF0A5B] flex items-center justify-center flex-wrap">
-      <div className="container mx-auto">
-      <div className="">
           <Navbar />
-        </div>
-      <img className="w-full h-[110px] sm:h-[161px]" src={group1} alt="group1" />
+        <img className="w-full h-[90px] sm:h-[150px]" src={group1} alt="group1" />
+      <div className="container mx-auto">
 
-        <img className="w-[350px] h-[120px] mt-[80px] sm:ml-[89px]" src={pic1} alt="pic1" />
+        <img className="w-[200px] h-[90px] m-auto mt-[50px] sm:w-[320px] sm:h-[150px] sm:mt-[70px] sm:ml-[89px]" src={pic1} alt="pic1" />
 
-        <div className="flex flex-row items-start justify-between flex-wrap w-full py-[30px] sm:flex-nowrap">
-          <p className="flex items-center text-[#FFF] text-4xl font-poppins w-full h-[769px] m-[20px] py-[100px] sm:w-1/2">
+        <div className="flex flex-row items-start flex-wrap w-full py-[30px] sm:flex-nowrap">
+          <p className="flex items-start text-[#FFF] text-2xl text-justify font-poppins leading-normal w-full h-auto sm:h-[769px] m-[20px] py-[20px] sm:w-1/2 sm:text-4xl sm:py-[200px]">
             Madhuram'25 is SLIET Longowal’s premier social and cultural festival, uniting over 10,000 attendees in a grand celebration of music, dance, art, and creativity. This two-day extravaganza showcases exciting events, performances, and competitions, making it a vibrant platform for talent, culture, and unforgettable experiences.
           </p>
-          <img className="w-full h-[770px] sm:w-1/2" src={group} alt="Group" />
+          <img className="w-full h-[700px] sm:w-1/2" src={group} alt="Group" />
         </div>
 
         <div className="flex justify-center">
-          <img className="ml-4 w-[350px] h-[150px]" src={design3} alt="Design3" />
+          <img className="ml-4 w-[200px] h-[90px] sm:w-[320px] sm:h-[150px]" src={design3} alt="Design3" />
         </div>
 
-        <div className="flex items-center py-6 justify-around sm:flex sm:items-center sm:justify-between">
+        <div className="flex items-center py-10 justify-around sm:flex sm:items-center sm:justify-between">
           <button onClick={prevSlide} aria-label="Previous Slide" className="flex items-center">
             <img className="mx-2 w-[50px] h-[50px]" src={leftarrow} alt="Previous" />
           </button>
@@ -97,14 +96,11 @@ function About() {
           </button>
         </div>
 
-        <img className="w-full h-[110px] sm:h-[161px]" src={groupbottom} alt="Group Bottom" />
+        <img className="w-full h-[90px] sm:h-[161px]" src={groupbottom} alt="Group Bottom" />
         <img className="w-full h-full bg-[#fff]" src={group2} alt="Group 2" />
-
-        <div className="">
-          <Footer />
-        </div>
-
         
+        {/* footer component here */}
+
         <img className="w-full h-full bg-[#fff]" src={group2} alt="Group 2" />
       </div>
     </div>
