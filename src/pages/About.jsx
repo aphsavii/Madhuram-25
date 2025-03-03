ce8076659667ccc9105bcabe4b0445caeb989b53
Rahul Sharma <moudgil010@gmail.com>
2025-03-03 19:12:04 +0530

    About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 import group1 from "/assets/About/group1.png";
 import pic1 from "/assets/About/pic1.png";
 import group from "/assets/About/group.png";
@@ -13,7 +15,7 @@ import gpic3 from "/assets/About/gpic3.jpg";
 import { transform } from "motion";
 
 function About() {
-  const images = [gpic2, gpic2, gpic2, gpic3, gpic3, gpic1, gpic1, gpic1];
+  const images = [gpic2, gpic2, gpic2, gpic3, gpic3, gpic1, gpic1, gpic1];   // images should be multiple of 3
   const [currentIndex, setCurrentIndex] = useState(0);
   const [photosToShow, setPhotosToShow] = useState(1);
 
@@ -34,29 +36,26 @@ function About() {
 
   const nextSlide = () => {
     setCurrentIndex((prevIndex) => {
-      let newIndex = prevIndex + 1;
-      if (newIndex > images.length - photosToShow) {
-        newIndex = 0; 
-      }
-      return newIndex;
+      const newIndex = prevIndex + 1;
+      return newIndex > images.length - photosToShow ? 0 : newIndex;
     });
   };
-  
+
   const prevSlide = () => {
     setCurrentIndex((prevIndex) => {
-      let newIndex = prevIndex - 1;
-      if (newIndex < 0) {
-        newIndex = images.length - photosToShow; 
-      }
-      return newIndex;
+      const newIndex = prevIndex - 1;
+      return newIndex < 0 ? images.length - photosToShow : newIndex;
     });
   };
 
 
   return (
     <div className="w-full bg-[#FF0A5B] flex items-center justify-center flex-wrap">
-        <img className="w-full h-[110px] sm:h-[161px]" src={group1} alt="group1" />
       <div className="container mx-auto">
+      <div className="">
+          <Navbar />
+        </div>
+      <img className="w-full h-[110px] sm:h-[161px]" src={group1} alt="group1" />
 
         <img className="w-[350px] h-[120px] mt-[80px] sm:ml-[89px]" src={pic1} alt="pic1" />
 
@@ -76,24 +75,19 @@ function About() {
             <img className="mx-2 w-[50px] h-[50px]" src={leftarrow} alt="Previous" />
           </button>
 
-          <div className="w-full flex items-center justify-center overflow-hidden relative sm:w-[1200px] sm:overflow-hidden sm:relative">
+          <div className="w-[300px] sm:w-[920px] sm:mx-2 flex items-center justify-between overflow-hidden relative">
             <div
-              className="flex"
-              style={{
-                transition: "transform 500ms ease-in-out",
-                transform: `translateX(-${currentIndex+1})`,
-                "@media (min-width: 800px)": {
-                  transition: "transform 500ms ease-in-out",
-                  transform: `translateX(-${currentIndex * 500 }px)`,
-                },
-              }}>
+              className="flex transition-transform duration-500 ease-in-out"
+              style={{ transform: `translateX(-${currentIndex * 300}px)` }}
+            >
               {images.map((image, index) => (
-                <img
-                  key={index}
-                  className="w-[350px] h-[350px] sm:mx-[30px] sm:w-[350px] sm:[450px]"
-                  src={image}
-                  alt={`Slide ${index + 1}`}
-                />
+                <div key={index} className="w-[300px] h-[300px] sm:w-[300px] sm:h-[330px]">
+                  <img
+                    className="w-full h-full sm:px-8"
+                    src={image}
+                    alt={`Slide ${index}`}
+                  />
+                </div>
               ))}
             </div>
           </div>
@@ -106,7 +100,9 @@ function About() {
         <img className="w-full h-[110px] sm:h-[161px]" src={groupbottom} alt="Group Bottom" />
         <img className="w-full h-full bg-[#fff]" src={group2} alt="Group 2" />
 
-        <div className="text-center py-4">Contact Us from home page</div>
+        <div className="">
+          <Footer />
+        </div>
 
         
         <img className="w-full h-full bg-[#fff]" src={group2} alt="Group 2" />
