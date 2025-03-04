d64050415fdd1f36dea187f62756aa640f6de098
Rahul Sharma <moudgil010@gmail.com>
2025-03-04 23:59:34 +0530

    About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,10 +13,10 @@ import gpic2 from "/assets/About/gpic2.png";
 import gpic3 from "/assets/About/gpic3.jpg";
 
 function About() {
-  const images = [gpic2, gpic2, gpic2, gpic3, gpic3, gpic1, gpic1, gpic1]; // should be multiple of 3
+  const images = [gpic2, gpic1, gpic3, gpic1, gpic3, gpic2, gpic3, gpic1,gpic2]; // should be multiple of 3
   const [currentIndex, setCurrentIndex] = useState(0);
   const [photosToShow, setPhotosToShow] = useState(1);
-  const imageWidth = photosToShow === 3 ? 450 : 290; // Adjust width dynamically
+  const imageWidth = photosToShow === 3 ? 450 : 340; // Adjust width dynamically
 
   useEffect(() => {
     const handleResize = () => {
@@ -47,10 +47,10 @@ function About() {
       <img className="w-full h-auto" src={group1} alt="group1" />
 
       <div className="container mx-auto">
-        <img className="w-64 h-auto m-auto mt-[50px] md:w-96 sm:mt-[70px] sm:ml-[89px]" src={pic1} alt="pic1" />
+        <img className="w-64 h-auto m-auto mt-[50px] md:w-96 lg:mt-[70px] lg:ml-[89px]" src={pic1} alt="pic1" />
 
-        <div className="flex flex-col md:flex-row justify-between items-center flex-wrap w-full py-8 lg:py-0 sm:flex-nowrap mb-5 lg:mb-10">
-          <div className="flex items-center text-white text-2xl font-poppins lg:leading-[48px] h-auto  m-[20px] py-[20px] sm:w-1/2 sm:text-4xl text-center md:text-justify">
+        <div className="flex flex-col md:flex-row justify-between items-center flex-wrap w-full py-8 lg:py-0 lg:flex-nowrap mb-5 lg:mb-10">
+          <div className="flex items-center text-white text-2xl font-poppins lg:leading-[48px] h-auto  m-[20px] py-[20px] lg:w-1/2 lg:text-4xl text-center md:text-justify">
             Madhuram'25 is SLIET Longowal’s premier social and cultural festival, uniting over 10,000 attendees in a grand celebration of music, dance, art, and creativity. This two-day extravaganza showcases exciting events, performances, and competitions, making it a vibrant platform for talent, culture, and unforgettable experiences.
           </div>
           <img className="w-[500px] h-auto" src={group} alt="Group" />
@@ -63,24 +63,24 @@ function About() {
         {/* Carousel */}
         <div className="flex flex-row gap-2 w-full  py-10 justify-center lg:mb-20">
           <button onClick={prevSlide} aria-label="Previous Slide" className="flex items-center">
-            <img className="w-[43px] h-[38px] p-[5px] sm:w-[60px] sm:h-[52px] sm:p-[6px]" src={leftarrow} alt="Previous" />
+            <img className="w-[43px] h-[38px] p-[5px] lg:w-[60px] lg:h-[52px] lg:p-[6px]" src={leftarrow} alt="Previous" />
           </button>
 
-          <div className="w-full mx-auto flex items-center justify-center overflow-hidden relative">
+          <div className="w-full mx-auto flex flex-row items-center justify-start overflow-hidden relative lg:w-[1350px]">
             <div
               className="flex transition-transform duration-500 ease-in-out"
               style={{ transform: `translateX(-${currentIndex * imageWidth}px)` }}
             >
               {images.map((image, index) => (
-                <div key={index} className="w-68 lg:w-[450px] h-96 flex-shrink-0">
-                  <img className="w-full h-full object-cover sm:px-8" src={image} alt={`Slide ${index}`} />
+                <div key={index} className="w-[340px] h-[340px] lg:w-[450px] lg:h-96 flex-shrink-0">
+                  <img className="w-full h-full lg:px-8" src={image} alt={`Slide ${index}`} />
                 </div>
               ))}
             </div>
           </div>
 
           <button onClick={nextSlide} aria-label="Next Slide" className="flex items-center">
-            <img className="w-[43px] h-[38px] p-[5px] sm:w-[60px] sm:h-[52px] sm:p-[6px]" src={rightarrow} alt="Next" />
+            <img className="w-[43px] h-[38px] p-[5px] lg:w-[60px] lg:h-[52px] lg:p-[6px]" src={rightarrow} alt="Next" />
           </button>
         </div>
 
