2cb47e907e41aeb96053aa784356d8e6888507e6
Riyaz-ahmad11 <0502riyazahmad@gmail.com>
2025-03-11 00:44:41 +0530

    added coming soon Guidelines Page

--- src/pages/Guidelines.jsx
+++ src/pages/Guidelines.jsx
@@ -1,9 +1,7 @@
-
 import { motion } from "framer-motion";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import FlowerImage from "/assets/registerFooterFlower.svg";
-
 
 const Guidelines = () => {
   return (
@@ -22,22 +20,17 @@ const Guidelines = () => {
                 backgroundImage: "url('/assets/UpperBanner.png')",
               }}
             ></motion.div>
-            <motion.div
-              initial={{ opacity: 0, y: -20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ type: "spring", stiffness: 100, damping: 2 }}
-            >
-             
-            </motion.div>
           </div>
-         <div className="h-72 w-screen  flex items-center justify-center text-white font-bold text-3xl">Coming Soon...</div>
-          <div
-            className="w-full h-20 bg-repeat-x bg-[length:auto_100%] bg-top rotate-180"
-            style={{
-              backgroundImage: "url('/assets/UpperBanner.png')",
-            }}
-          ></div>
+          <div className="h-72 w-screen  flex items-center justify-center text-white font-bold text-3xl">
+            Coming Soon...
+          </div>
         </div>
+        <div
+          className="w-full h-20 bg-repeat-x bg-[length:auto_100%] bg-top rotate-180"
+          style={{
+            backgroundImage: "url('/assets/UpperBanner.png')",
+          }}
+        ></div>
 
         <Footer
           bgColor="#135871"
