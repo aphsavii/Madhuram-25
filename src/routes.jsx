c2637eb6e9959fb39c0ecf160721c941d6836be6
Avinash Kumar <avinash.k95347@gmail.com>
2025-03-29 14:12:21 +0530

    fix

--- src/routes.jsx
+++ src/routes.jsx
@@ -9,44 +9,26 @@ import HomePage from "./pages/HomePage";
 import Events from "./pages/Events";
 
 import Team from "./pages/Team";
+import Dashboard from "./pages/live-voting/Dashboard";
+import Analytics from "./pages/live-voting/Analytics";
+import Login from "./pages/live-voting/Login";
+
 const Routes = createRoutesFromElements(
-  <Route path="/" element={<  Layout />}>
-
+  <Route path="/" element={<Layout />}>
     {/* Routes for different pages */}
-    <Route
-      path="/"
-      element={
-       <HomePage />
-      }
-    />
-    <Route
-      path="/register"
-      element={
-       <Register />
-      }
-    />
-    
-     <Route
-      path="about"
-      element={
-        <About />
-      }
-    /> 
-    <Route
-      path="team"
-      element={
-       <Team />
-      }
-    />
-    <Route
-      path="events"
-      element={
-       <Events />
-      }
-    />
+    <Route path="/" element={<HomePage />} />
+    <Route path="/register" element={<Register />} />
 
+    <Route path="about" element={<About />} />
+    <Route path="team" element={<Team />} />
+    <Route path="events" element={<Events />} />
+    <Route path="dashboard" element={<Dashboard />} />
+    <Route path="analytics" element={<Analytics />} />
+
+    <Route path="/login" element={<Login />} />
+
+    <Route path="*" element={<Home />} />
     {/* Add other pages below */}
-
   </Route>
 );
 
